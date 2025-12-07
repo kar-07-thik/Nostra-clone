5e5cef258fdc9e9b9c62091b9a8b4d605be5a6af
kar-07-thik <karthi533110@gmail.com>
2025-12-07 13:58:47 +0530

    update code

--- script.js
+++ script.js
@@ -9,3 +9,53 @@
             navSlide.style.display = 'none'
             console.log(navSlideClose)
         })
+        var adClose = document.getElementById("top__ad__close")
+     adClose.addEventListener("click",function(){
+        adClose.parentNode.style.display= 'none'
+     })
+  
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var slidermargin=0
+
+console.log(sliderleftbutton)
+
+sliderrightbutton.addEventListener("click",
+
+function(){
+    slidermargin=slidermargin+100
+
+    if(slidermargin>200)
+    {
+        slidermargin=0
+        sliderimage.style.marginLeft=0;
+    }
+    else{
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+   
+
+}
+)
+
+sliderleftbutton.addEventListener("click",
+
+function(){
+
+    if(slidermargin==0)
+    {
+       
+        slidermargin=200
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    else{
+
+        slidermargin=slidermargin-100
+    sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+    
+}
+)
