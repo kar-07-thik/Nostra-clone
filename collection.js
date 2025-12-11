c836da1e4ed080e4c1a0aa7d5e937bb12dd82fa8
kar-07-thik <karthi533110@gmail.com>
2025-12-11 10:38:33 +0530

    update code with js

---
+++ collection.js
@@ -0,0 +1,28 @@
+var navIcon = document.getElementById("navicon")
+var navSlide = document.querySelector(".navbar__slide")
+var navSlideClose = document.querySelector(".navbar__slide__close")
+navIcon.addEventListener("click", function () {
+    navSlide.style.left = "0%"
+
+    console.log(navIcon)
+})
+navSlideClose.addEventListener("click", function () {
+    navSlide.style.left = '-45%'
+    console.log(navSlideClose)
+})
+var search = document.getElementById("searchbox")
+var container = document.getElementById("productcontainer")
+var productList = container.querySelectorAll("div")
+search.addEventListener("keyup", function () {
+    var entervalue = event.target.value.toUpperCase()
+    for (count = 0; count < productList.length; count = count + 1) {
+        var productname = productList[count].querySelector("h1").textContent.toUpperCase()
+        if (productname.indexOf(entervalue) < 0) {
+            productList[count].style.display = 'none'
+        }
+        else {
+            productList[count].style.display = 'block'
+        }
+    }
+})
+
---
+++ index.js
@@ -0,0 +1,61 @@
+var navIcon = document.getElementById("navicon")
+var navSlide = document.querySelector(".navbar__slide")
+var navSlideClose = document.querySelector(".navbar__slide__close")
+navIcon.addEventListener("click", function () {
+    navSlide.style.left = "0%"
+
+    console.log(navIcon)
+})
+navSlideClose.addEventListener("click", function () {
+    navSlide.style.left = '-45%'
+    console.log(navSlideClose)
+})
+var adClose = document.getElementById("top__ad__close")
+adClose.addEventListener("click", function () {
+    adClose.parentNode.style.display = 'none'
+})
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var margin=0
+sliderrightbutton.addEventListener("click",function(){
+    margin=margin+100
+    if(margin>200){
+        margin=0
+        sliderimage.style.marginLeft=0;
+    }
+       else{
+        sliderimage.style.marginLeft="-"+margin+"vw";
+    }
+})
+// var sliderleftbutton = document.getElementById("slider-left-activate")
+// var sliderrightbutton =document.getElementById("slider-right-activate")
+// var sliderimage= document.querySelector(".slider-image-container")
+// var slidermargin=0
+// console.log(sliderleftbutton)
+// sliderrightbutton.addEventListener("click",
+// function(){
+//     slidermargin=slidermargin+100
+//     if(slidermargin>200)
+//     {
+//         slidermargin=0
+//         sliderimage.style.marginLeft=0;
+//     }
+//     else{
+//         sliderimage.style.marginLeft="-"+slidermargin+"vw";
+//     }
+// }
+// )
+// sliderleftbutton.addEventListener("click",
+// function(){
+//     if(slidermargin==0)
+//     {
+//         slidermargin=200
+//         sliderimage.style.marginLeft="-"+slidermargin+"vw";
+//     }
+//     else{
+//         slidermargin=slidermargin-100
+//     sliderimage.style.marginLeft="-"+slidermargin+"vw";
+//     }
+// }
+// ) 
